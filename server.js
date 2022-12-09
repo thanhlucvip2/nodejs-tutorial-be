f6434ed4d51872c0c2856a787e29b98d6898d816
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2022-12-09 11:55:19 +0700

    public file

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const router1 = require("./router/account-router");
+const path = require("path");
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(
+  "/publicFile",
+  express.static(path.join(__dirname, "/public")) // dường dẩn public file
+);
 const port = 3000;
 app.use("/api/v1/", router1);
 app.get("/", (req, res) => {
-  res.send("test");
+  res.sendFile(path.join(__dirname, "hello.html")); // gửi file html cho phía client
 });
 
 app.listen(port, () => {
